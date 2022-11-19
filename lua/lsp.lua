e593cd037c90f1c41942d1fa9e959b60f260a164
tomas-mota <tomasrebelomota@gmail.com>
2022-11-19 10:06:26 +0100

    add more lsp shortcuts

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -10,6 +10,9 @@ require'lspconfig'.gopls.setup{
         map_key("n", "gi", vim.lsp.buf.implementation)
         map_key("n", "gr", vim.lsp.buf.references)
         map_key("n", "<leader>r", vim.lsp.buf.rename)
+        map_key("n", "<leader>e", vim.diagnostic.goto_next)
+        map_key("n", "<leader>le", "<cmd>Telescope diagnostics<cr>")
+        map_key('n', '<C- >', ':lua vim.lsp.buf.code_action()<CR>')
 
         -- go.nvim setup
         vim.api.nvim_create_autocmd("BufWritePre", {
