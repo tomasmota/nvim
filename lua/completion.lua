ea6c12654090818402b9a2fe0219f71b5cdb617e
tomas-mota <tomasrebelomota@gmail.com>
2022-11-21 22:14:55 +0100

    fix autopairs

--- lua/completion.lua
+++ lua/completion.lua
@@ -76,3 +76,10 @@ require('lspconfig')['gopls'].setup {
         usePlaceholders = true,
     }
 }
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
