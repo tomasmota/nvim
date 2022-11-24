9373ab1a1b9023cabd60ff39ff4d66b600ec31c0
Thomas Mota <tmm@danskecommodities.com>
2022-11-24 15:48:49 +0100

    auto open telescope

--- lua/settings.lua
+++ lua/settings.lua
@@ -68,6 +68,21 @@ telescope.setup({
       },
   })
 
+-- Open Telescope find_files when opening nvim with a directory as first argument
+_G.open_telescope = function()
+    local first_arg = vim.v.argv[2]
+    if first_arg and vim.fn.isdirectory(first_arg) == 1 then
+        vim.g.loaded_netrw = true
+        require("telescope.builtin").find_files({search_dirs = {first_arg}})
+    end
+end
+vim.api.nvim_exec([[
+augroup TelescopeOnEnter
+    autocmd!
+    autocmd VimEnter * lua open_telescope()
+augroup END
+]], false)
+
 require('Comment').setup()
 require('git').setup()
 require('nvim-autopairs').setup()
