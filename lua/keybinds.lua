3765369b191e6f5520c7016c5ccbbfa52073270d
tomas-mota <tomasrebelomota@gmail.com>
2022-11-26 22:40:34 +0100

    organize bindings
    Signed-off-by: tomas-mota <tomasrebelomota@gmail.com>

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -5,26 +5,35 @@ local function map_key(m, k, v)
     vim.keymap.set(m, k, v, { noremap = true, silent = true })
 end
 
--- Save with ctrl-s
-map_key('n', '<C-s>', ':w<cr>')
-map_key('n', '<leader>as', ':ASToggle<CR>')
+-- Save with <leader>s
+map_key('n', '<leader>s', ':w<CR>')
 
--- Mimic shell movements
+-- - Mimic shell movements
 map_key('i', '<C-E>', '<ESC>A')
 map_key('i', '<C-A>', '<ESC>I')
 
+-- Telescope stuff
 local builtin = require('telescope.builtin')
-map_key('n', '<c-p>', function() builtin.find_files { hidden = true } end, {}) 
--- map_key('n', '<c-p>', builtin.find_files(opts), {}) 
-map_key('n', '<leader>fa', builtin.live_grep, {})
-map_key('n', '<leader>fk', builtin.keymaps, {})
-map_key('n', '<leader>fs', builtin.lsp_document_symbols, {})
-map_key('n', '<leader>fb', require('telescope').extensions.file_browser.file_browser, { noremap = true }) -- file browser
--- find out how to upen file_browser inside the vim config from anywhere
-map_key('n', '<leader>h', builtin.help_tags, {})
-map_key('n', '<leader>gs', builtin.git_status, {})
-map_key('n', '<C-f>', builtin.current_buffer_fuzzy_find, {})
+map_key('n', '<C-p>', function() builtin.find_files { hidden = true } end) 
+map_key('n', '<leader>fa', builtin.live_grep)
+map_key('n', '<leader>fk', builtin.keymaps)
+map_key('n', '<leader>fs', builtin.lsp_document_symbols)
+map_key('n', '<leader>fb', require('telescope').extensions.file_browser.file_browser) -- file browser
+map_key("n", "<leader>le", "<cmd>Telescope diagnostics<cr>")
+map_key('n', '<leader>h', builtin.help_tags)
+map_key('n', '<leader>gs', builtin.git_status)
+map_key('n', '<C-f>', builtin.current_buffer_fuzzy_find)
 
 local ls = require("luasnip")
 map_key({ "i", "s" }, "<C-l>", ls.expand_or_jump)
 map_key({ "i", "s" }, "<C-h>", function() ls.jump(-1) end)
+
+-- lsp stuff
+map_key("n", "K", vim.lsp.buf.hover)
+map_key("n", "gd", vim.lsp.buf.definition)
+map_key("n", "gD", vim.lsp.buf.declaration)
+map_key("n", "gi", vim.lsp.buf.implementation)
+map_key("n", "gr", vim.lsp.buf.references)
+map_key("n", "<leader>r", vim.lsp.buf.rename)
+map_key("n", "<leader>e", vim.diagnostic.goto_next)
+map_key('n', '<C- >', ':lua vim.lsp.buf.code_action()<CR>')
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,20 +1,5 @@
-local function map_key(m, k, v)
-    vim.keymap.set(m, k, v, { noremap = true, silent = true })
-end
-
 require'lspconfig'.gopls.setup{
-
     on_attach = function()
-        map_key("n", "K", vim.lsp.buf.hover)
-        map_key("n", "gd", vim.lsp.buf.definition)
-        map_key("n", "gD", vim.lsp.buf.declaration)
-        map_key("n", "gi", vim.lsp.buf.implementation)
-        map_key("n", "gr", vim.lsp.buf.references)
-        map_key("n", "<leader>r", vim.lsp.buf.rename)
-        map_key("n", "<leader>e", vim.diagnostic.goto_next)
-        map_key("n", "<leader>le", "<cmd>Telescope diagnostics<cr>")
-        map_key('n', '<C- >', ':lua vim.lsp.buf.code_action()<CR>')
-
         -- go.nvim setup
         vim.api.nvim_create_autocmd("BufWritePre", {
           pattern = "*.go",
@@ -24,27 +9,16 @@ require'lspconfig'.gopls.setup{
           group = format_sync_grp,
         })
         require('go').setup()
-
     end,
 }
 
-
 require'lspconfig'.terraformls.setup{
     on_attach = function()
-        map_key("n", "K", vim.lsp.buf.hover)
-        map_key("n", "gd", vim.lsp.buf.definition)
-        map_key("n", "gD", vim.lsp.buf.declaration)
-        map_key("n", "gi", vim.lsp.buf.implementation)
-        map_key("n", "gr", vim.lsp.buf.references)
-        map_key("n", "<leader>r", vim.lsp.buf.rename)
-        map_key("n", "<leader>e", vim.diagnostic.goto_next)
-        map_key("n", "<leader>le", "<cmd>Telescope diagnostics<cr>")
-        map_key('n', '<C- >', ':lua vim.lsp.buf.code_action()<CR>')
-
         vim.o.tabstop = 2
         vim.o.softtabstop = 2
         vim.o.shiftwidth = 2
         vim.o.expandtab = true
-
     end,
 }
+
+require'lspconfig'.luau_lsp.setup{}
