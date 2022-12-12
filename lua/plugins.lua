35185ad930e21430d9dacdd8b17e39d3f1861154
tomas-mota <tomasrebelomota@gmail.com>
2022-12-12 16:59:48 +0100

    add which-key
    Signed-off-by: tomas-mota <tomasrebelomota@gmail.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -70,5 +70,16 @@ return require('packer').startup(function(use)
         require('neoclip').setup()
       end,
     }
+
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
 end)
 
